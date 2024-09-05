b58a054b413a2b5e6e17e053ad5a1f2649412d03
Rohan Samuel <rohansamuelx07@gmail.com>
2024-09-06 01:31:42 +0530

    creating pages and routes

---
+++ client/src/components/Header.jsx
@@ -0,0 +1,7 @@
+import { Navbar } from "flowbite-react";
+
+export default function Header() {
+  return (
+    <Navbar>Header</Navbar>
+  )
+}
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,9 +1,29 @@
+// /** @type {import('tailwindcss').Config} */
+// export default {
+//   content: [
+//     "./index.html",
+//     "./src/**/*.{js,ts,jsx,tsx}",
+//   ],
+//   theme: {
+//     extend: {},
+//   },
+//   plugins: [],
+// }
 /** @type {import('tailwindcss').Config} */
+
+/** @type {import('tailwindcss').Config} */
+import flowbitePlugin from 'flowbite/plugin';
+
 export default {
-  content: [],
+  content: [
+    './index.html',
+    './src/**/*.{js,ts,jsx,tsx}',
+    'node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}',
+  ],
   theme: {
     extend: {},
   },
-  plugins: [],
-}
-
+  plugins: [
+    flowbitePlugin,
+  ],
+};
