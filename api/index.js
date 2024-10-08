d930a0158fa4a6aa8e626bf945285514eb39292b
Rohan Samuel <rohansamuelx07@gmail.com>
2024-10-08 21:04:33 +0530

    create signup API route

--- api/index.js
+++ api/index.js
@@ -2,7 +2,7 @@ import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRoutes from './routes/user.route.js';
-
+import authRoutes from './routes/auth.route.js';
 dotenv.config();
 
 mongoose.connect(process.env.MONGO
@@ -12,8 +12,11 @@ mongoose.connect(process.env.MONGO
     console.error('Error connecting to MongoDB Atlas!', err);
 });
 const app = express();
+
+app.use(express.json());
 app.listen(3000, () => {
     console.log('Server is running on port 3000!!');
 })
 
 app.use('/api/user', userRoutes);
+app.use('/api/auth', authRoutes);
