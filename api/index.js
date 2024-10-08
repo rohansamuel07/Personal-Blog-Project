5efe974c23dac5f1eddd762eef8884b568387291
Rohan Samuel <rohansamuelx07@gmail.com>
2024-10-08 19:31:21 +0530

    connect to the database

--- api/index.js
+++ api/index.js
@@ -1,5 +1,15 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
 
+dotenv.config();
+
+mongoose.connect(process.env.MONGO
+).then(() => {
+    console.log('Connected to MongoDB Atlas!');
+}).catch(err => {
+    console.error('Error connecting to MongoDB Atlas!', err);
+});
 const app = express();
 app.listen(3000, () => {
     console.log('Server is running on port 3000!!');
