0bffbbabae3878ae8415efb7dba4c333a29f1304
Rohan Samuel <rohansamuelx07@gmail.com>
2024-10-10 21:05:50 +0530

    complete sidebar of the dashboard

---
+++ client/src/components/DashSidebar.jsx
@@ -0,0 +1,30 @@
+
+import { Sidebar } from 'flowbite-react';
+import { HiUser, HiArrowSmRight } from 'react-icons/hi';
+import { Link } from 'react-router-dom';
+
+export default function DashSidebar() {
+  return (
+    <Sidebar className='w-full md:w-56'>
+      <Sidebar.Items>
+        <Sidebar.ItemGroup className='flex flex-col gap-1'>
+          <Link to='/dashboard?tab=profile'>
+            <Sidebar.Item
+              icon={HiUser}
+              label={ 'user' }
+              labelColor='dark'
+            >
+              Profile
+            </Sidebar.Item>
+          </Link>
+          <Sidebar.Item
+            icon={HiArrowSmRight}
+            className='cursor-pointer'
+          >
+            Sign Out
+          </Sidebar.Item>
+        </Sidebar.ItemGroup>
+      </Sidebar.Items>
+    </Sidebar>
+  );
+}
